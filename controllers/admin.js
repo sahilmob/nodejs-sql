3a51cc4086b628ac3d4618373571a58b3c1c0f32
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-08 17:18:56 +0200

    add logic to edit product

--- controllers/admin.js
+++ controllers/admin.js
@@ -32,17 +32,19 @@ exports.getEditProduct = (req, res, next) => {
 		return res.redirect("/");
 	}
 	const prodId = req.params.productId;
-	Product.findById(prodId, product => {
-		if (!product) {
-			return res.redirect("/");
-		}
-		res.render("admin/edit-product", {
-			pageTitle: "Edit Product",
-			path: "/admin/edit-product",
-			editing: editMode,
-			product: product
-		});
-	});
+	Product.findById(prodId)
+		.then(product => {
+			if (!product) {
+				return res.redirect("/");
+			}
+			res.render("admin/edit-product", {
+				pageTitle: "Edit Product",
+				path: "/admin/edit-product",
+				editing: editMode,
+				product: product
+			});
+		})
+		.catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -51,15 +53,21 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedPrice = req.body.price;
 	const updatedImageUrl = req.body.imageUrl;
 	const updatedDesc = req.body.description;
-	const updatedProduct = new Product(
-		prodId,
-		updatedTitle,
-		updatedImageUrl,
-		updatedDesc,
-		updatedPrice
-	);
-	updatedProduct.save();
-	res.redirect("/admin/products");
+	Product.findById(prodId)
+		.then(product => {
+			product.title = updatedTitle;
+			product.price = updatedPrice;
+			product.imageUrl = updatedImageUrl;
+			product.description = updatedDesc;
+			return product.save();
+		})
+		//We can chain then because we returned products.save();
+		.then(result => {
+			console.log("Product updated");
+			//We added redirect here to redirect only after promise resolve and hence reflect updated product data on the products page
+			res.redirect("/admin/products");
+		})
+		.catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
