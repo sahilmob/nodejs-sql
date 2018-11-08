07c6ed43e98b287d5084302a9b970f1e0e28e7c0
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-08 18:16:50 +0200

    define relationship between user and product

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const Cart = require("./models/cart");
+const User = require("./models/user");
 
 const app = express();
 
@@ -22,8 +25,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
-	.sync()
+	.sync({ force: true })
 	.then(result => {
 		// console.log(result);
 		app.listen(3000);
