d82eb0a80622d266d2848a425a4ad60dd60997b8
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-08 21:39:05 +0200

    render orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -134,10 +134,16 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-	res.render("shop/orders", {
-		path: "/orders",
-		pageTitle: "Your Orders"
-	});
+	req.user
+		.getOrders({ include: ["products"] })
+		.then(orders => {
+			res.render("shop/orders", {
+				path: "/orders",
+				pageTitle: "Your Orders",
+				orders
+			});
+		})
+		.catch(err => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
