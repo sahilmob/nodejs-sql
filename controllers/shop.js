99ed8b429122e04931224e3ad6a007e549a2d883
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-08 17:05:41 +0200

    add logic to get single product

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,9 +14,9 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
 	Product.findById(prodId)
-		.then(([product]) => {
+		.then(product => {
 			res.render("shop/product-detail", {
-				product: product[0],
+				product,
 				pageTitle: product.title,
 				path: "/products"
 			});
